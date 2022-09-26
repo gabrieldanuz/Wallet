fbcfc2492e29cec2abfb88e240b775da74a0e95f
Gabriel Oliveira <gabrieldanuzoliveira358@gmail.com>
2022-09-26 19:37:17 -0300

    Tomando forma

--- App.tsx
+++ App.tsx
@@ -1,12 +1,45 @@
 import React from "react";
+import AppLoading from "expo-app-loading";
 import { View, Text } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+
+import {
+  useFonts,
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+  Poppins_800ExtraBold
+} from '@expo-google-fonts/poppins';
+
+import { DMSans_400Regular } from '@expo-google-fonts/dm-sans';
+import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
+import { Colors } from "react-native/Libraries/NewAppScreen";
+
+import COLORS from "./src/styles/theme";
+
 
 const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+    DMSans_400Regular,
+    DMSerifDisplay_400Regular
+  })
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  }
 
   return (
-  <View>
-    <Text>Wallet</Text>
-  </View>
+    <ThemeProvider theme={COLORS}>
+      <View>
+        <Text>Wallet</Text>
+      </View>
+  </ThemeProvider>
   )
 }
 
